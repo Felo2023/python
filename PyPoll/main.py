08b81e5db810774b075d5c7f848e8c9ffb86f7b7
Felo2023 <134896499+Felo2023@users.noreply.github.com>
2023-06-21 20:28:03 -0400

    PyPoll completed

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -15,12 +15,11 @@ with open ('Resources/election_data.csv') as file:
         i=i+1
     
 total_votes=str(len(ballot_id))
-print(len(ballot_id))
 
 'Ensure that the candidates names are sorted and add a blank cell'
 candidate2=sorted(candidate)
 candidate2.append('')
-'Find different candidates'
+'Find different candidates and their quantity of votes'
 persons=[]
 votes=[]
 vote=0
@@ -38,11 +37,43 @@ for person in candidate2:
     else:
         vote=vote+1
 persons.pop()
-print(persons)
-print(votes)
+votes_percent=[]
+
+'Transform votes into %'
+for i in votes:
+    votes_percent.append(round(i*100/len(ballot_id),3))
+
+'Find index that has higher votes_percent'
+winner_index=0
+winner=votes_percent[0]
+for x in votes_percent:
+    if x>winner:
+        winner=x
+        winner_index=winner_index+1
+
+'print on terminal'
+u=0
+print('Election Results' +'\n'+'\n')
+print('-------------------------' +'\n'+'\n')
+print('Total Votes: '+ total_votes + '\n'+'\n')
+print('-------------------------' +'\n'+'\n')
+for person in persons:
+    print(persons[u]+': '+ str(votes_percent[u])+'% '+'('+str(votes[u])+')'+'\n'+'\n')
+    u=u+1
+print('-------------------------' +'\n'+'\n')
+print('Winner: '+persons[winner_index]+'\n'+'\n')
+print('-------------------------' +'\n'+'\n')
+
 'Write results on text file'
+u=0
 with open ('Analysis/Result.txt','w') as result:
     result.write('Election Results' +'\n'+'\n')
     result.write('-------------------------' +'\n'+'\n')
     result.write('Total Votes: '+ total_votes + '\n'+'\n')
-    result.writelines(candidate[1])
+    result.write('-------------------------' +'\n'+'\n')
+    for person in persons:
+        result.write(persons[u]+': '+ str(votes_percent[u])+'% '+'('+str(votes[u])+')'+'\n'+'\n')
+        u=u+1
+    result.write('-------------------------' +'\n'+'\n')
+    result.write('Winner: '+persons[winner_index]+'\n'+'\n')
+    result.write('-------------------------' +'\n'+'\n')
